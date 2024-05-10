564746e4d65b6b9f1e2854aaaa6f66e7da4d5706
Dory Rutayisire <doryrutayisire@Irembo-171-DRutayisire.local>
2024-05-10 11:47:51 +0200

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""
+Define a function
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """ Concact Two Strings Together """
+    return a + b
+    """ Return The Concatenated String """
