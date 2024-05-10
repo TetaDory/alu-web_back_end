6d90662a09c5736d3ea79970e37334806f84c2c2
Dory Rutayisire <doryrutayisire@Irembo-171-DRutayisire.local>
2024-05-10 11:53:22 +0200

    1-concat.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+""" import modules """
+import math
+
+"""
+Define a function
+"""
+
+
+def floor(n: float) -> int:
+    """ Find The Floor Of the Number N """
+    return math.floor(n)
+    """ Return The Floor Of n """
