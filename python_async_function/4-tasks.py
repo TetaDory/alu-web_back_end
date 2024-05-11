72d3b65718fd15971b1f9247cdac493b3d7a398e
TetaDory <113389069+TetaDory@users.noreply.github.com>
2024-05-11 09:33:09 +0200

    Update 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,12 +6,18 @@
 
 import asyncio
 from typing import List
+from asyncio import gather
 
-task_wait_random = __import__('3-tasks').task_wait_random
 
+async def task_wait_random(max_delay):
+    """Simulate A Random Wait. """
+    delay = randint(0, max_delay)
+    await asyncio.sleep(delay)
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ New Function """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
-    """ Return Async Function """
+async def task_wait_n(n, max_delay):
+    """ Waits for n tasks to complete. """
+    tasks = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(task_wait_random(max_delay)))
+    await asyncio.gather(*tasks)
+    """ Return Async Function. """
