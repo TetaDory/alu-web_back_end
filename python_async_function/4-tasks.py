11363c13bc62e2e8f284ee67470bf562f027e299
TetaDory <113389069+TetaDory@users.noreply.github.com>
2024-05-11 09:41:17 +0200

    Update 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,28 +6,27 @@
 
 import asyncio
 from typing import List
-from asyncio import gather
 
-
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-task_wait_random').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Function that waits random amount of time/delay """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    data_list = await gather(*tasks)
-    new_list = []
+    """
+    Calls task_wait_random n times with the specified max_delay and returns a list of the results.
 
-    while data_list:
-        minimum = data_list[0]  # arbitrary number in list
-        for x in data_list:
-            if x < minimum:
-                minimum = x
-        new_list.append(minimum)
-        data_list.remove(minimum)
+    Args:
+        n (int): The number of times to call task_wait_random.
+        max_delay (int): The maximum delay in seconds.
 
-    return new_list
+    Returns:
+        List[float]: A list containing the results of calling task_wait_random.
+    """
+    results = []
 
+    for _ in range(n):
+        results.append(await task_wait_random(max_delay))
+
+    return results
 
 if __name__ == "__main__":
     asyncio.run(task_wait_n)
