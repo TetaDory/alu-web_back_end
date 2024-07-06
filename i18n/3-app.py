1426de0ddb764fff6f7edf4d5dc263d7e806ffbd
Dory Rutayisire <doryrutayisire@Irembo-171-DRutayisire.local>
2024-07-06 22:36:54 +0200

    3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -17,11 +17,11 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
-
-@babel.localeselector
+ 
 def get_locale():
     """Get locale for your application"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
