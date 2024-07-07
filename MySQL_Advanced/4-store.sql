1fd601ebfb60423c28a004f458ffd5db32d3aee3
Dory Rutayisire <doryrutayisire@Irembo-171-DRutayisire.local>
2024-07-07 21:12:31 +0200

    exercise

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,4 +1,5 @@
---- Script to decrease quantity
+-- SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
 FOR EACH ROW UPDATE items
 SET
