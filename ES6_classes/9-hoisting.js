f7a2c3a13e1097f90520391d463795d688a9b22d
Dory Rutayisire <doryrutayisire@Irembo-171-DRutayisire.local>
2024-07-18 10:40:02 +0200

    1

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,15 +1,15 @@
 export class HolbertonClass {
   constructor(year, location) {
-      this._year = year;
-      this._location = location;
+    this._year = year;
+    this._location = location;
   }
 
   get year() {
-      return this._year;
+    return this._year;
   }
 
   get location() {
-      return this._location;
+    return this._location;
   }
 }
 
@@ -19,21 +19,21 @@ const class2020 = new HolbertonClass(2020, 'San Francisco');
 // studentHolberton.js
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-      this._firstName = firstName;
-      this._lastName = lastName;
-      this._holbertonClass = holbertonClass;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
-      return `${this._firstName} ${this._lastName}`;
+    return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
-      return this._holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
